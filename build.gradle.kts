beabaa2e3f888d18ab1ff001e82d3caa4ad2779a
Weber, Maximilian <maximilian.weber@epos-cat.de>
2026-03-13 07:54:40 +0100

    Rearrange some project files; Add plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("java")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "io.github.mxiwbr"
+version = "1.0.0"
 
 repositories {
     maven {
---
+++ src/main/java/io/github/mxiwbr/capturebioms/CaptureBioms.java
@@ -0,0 +1,12 @@
+package io.github.mxiwbr.capturebioms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CaptureBioms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabled!");
+    }
+
+}
